5020a71db9f763fdf66277f6f3f5558662e04f73
prantaroy11 <prantaroy10@gmail.com>
2026-03-15 23:13:41 +0530

    Fix:fix mailer

--- services/email.service.js
+++ services/email.service.js
@@ -15,8 +15,8 @@ const mailer = (userId, password) => {
     return nodemailer.createTransport({
 
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
 
         auth: {
             user: userId,
