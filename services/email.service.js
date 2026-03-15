3ae141b53597e4dae746a0af0813ddc77d455f0b
prantaroy11 <prantaroy10@gmail.com>
2026-03-16 01:59:29 +0530

    Fix:Refactor email service

--- services/email.service.js
+++ services/email.service.js
@@ -1,20 +1,49 @@
-const nodemailer=require('nodemailer');
-const { Resend } = require("resend");
+// const nodemailer=require('nodemailer');
+// const { Resend } = require("resend");
 
-const mailer=(userId,password)=>{
-    return nodemailer.createTransport({
-        service:'Gmail',
-        secure: true,
-        auth:{
-            user:userId,
-            pass:password
-        },
-        family: 4
-    });
+// const mailer=(userId,password)=>{
+//     return nodemailer.createTransport({
+//         service:'Gmail',
+//         secure: true,
+//         auth:{
+//             user:userId,
+//             pass:password
+//         },
+//         family: 4
+//     });
+// }
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+
+
+// module.exports=mailer;
+
+const { Resend } = require('resend');
+
+const mailer = () => {
+    // This automatically grabs the key you just added to Render
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    return {
+        sendMail: async (mailData, callback) => {
+            try {
+                const data = await resend.emails.send({
+                    from: 'onboarding@resend.dev', // Resend's testing email address
+                    to: mailData.to, 
+                    subject: mailData.subject,
+                    text: mailData.text
+                });
+
+                if (data.error) {
+                    return callback(data.error, null);
+                }
+                return callback(null, data);
+            } catch (err) {
+                return callback(err, null);
+            }
+        }
+    };
 }
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
-
-
-module.exports=mailer;
+module.exports = mailer;
