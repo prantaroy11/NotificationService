405d32b9e8a085af702cbbc6feae2b11a2c54c74
prantaroy11 <prantaroy10@gmail.com>
2026-03-16 00:10:40 +0530

    Fix:debugging mail issue

--- services/email.service.js
+++ services/email.service.js
@@ -39,14 +39,13 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (to, subject, content) => {
 
-  const response = await resend.emails.send({
-    from: process.env.EMAIL,
+  await resend.emails.send({
+    from: "prantaroy223@gmail.com",
     to: to,
     subject: subject,
     text: content
   });
 
-  return response;
 };
 
 module.exports = sendMail;
