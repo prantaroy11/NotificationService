3856fc9e3ebec123d4382b1b0c3754d3ab8d0965
prantaroy11 <prantaroy10@gmail.com>
2026-03-15 22:15:02 +0530

    Fix: emailService

--- services/email.service.js
+++ services/email.service.js
@@ -1,12 +1,30 @@
 const nodemailer=require('nodemailer');
 
-const mailer=(userId,password)=>{
+// const mailer=(userId,password)=>{
+//     return nodemailer.createTransport({
+//         service:'Gmail',
+//         auth:{
+//             user:userId,
+//             pass:password
+//         }
+//     });
+// }
+
+const mailer = (userId, password) => {
+
     return nodemailer.createTransport({
-        service:'Gmail',
-        auth:{
-            user:userId,
-            pass:password
+
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+
+        auth: {
+            user: userId,
+            pass: password
         }
+
     });
-}
+
+};
+
 module.exports=mailer;
